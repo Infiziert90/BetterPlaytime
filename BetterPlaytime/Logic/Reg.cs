3e127ad8b27cdffb50edef715c209defda118ca9
Infi <infiziert@protonmail.ch>
2022-09-27 13:06:12 +0200

    FIx french regex bug

--- BetterPlaytime/Logic/Reg.cs
+++ BetterPlaytime/Logic/Reg.cs
@@ -7,7 +7,7 @@ public static class Reg
 {
     private static Regex PlaytimeRegexEN = new Regex(@"^Total Play Time:(?: (?<days>\d+) days?,)?(?: (?<hours>\d+) hours?,)? (?<minutes>\d+) minutes?");
     private static Regex PlaytimeRegexDE = new Regex(@"^Gesamtspielzeit:(?: (?<days>\d+) Tage?,)?(?: (?<hours>\d+) Stunden?,)? (?<minutes>\d+) Minuten?");
-    private static Regex PlaytimeRegexFR = new Regex(@"^Temps de jeu total :(?: (?<days>\d+) jours?,)?(?: (?<hours>\d+) heures?,)? (?<minutes>\d+) minutes?");
+    private static Regex PlaytimeRegexFR = new Regex(@"^Temps de jeu total:(?: (?<days>\d+) jours?,)?(?: (?<hours>\d+) heures?,)? (?<minutes>\d+) minutes?");
     private static Regex PlaytimeRegexJP = new Regex(@"^累積プレイ時間 (?:(?<days>\d+)日)?(?:(?<hours>\d+)時間)?(?<minutes>\d+)分");
 
 
